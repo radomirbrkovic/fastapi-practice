d79c145e6fbbe919677c20f4368952cc5fc5977d
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-09-09 15:53:56 +0200

    Setup database conf

---
+++ app/infastructure/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from config import get_config
+
+
+conf = get_config("database")
+SQLALCHEMY_DATABASE_URL = f"postgresql://{conf['username']}:{conf['password']}@{conf['host']}/{conf['name']}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+Base = declarative_base()
