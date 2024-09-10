8fac5e8e4e1e224e4c376043e95f32e324c49f69
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-09-10 13:32:34 +0200

    Establish database connection

--- app/infastructure/database.py
+++ app/infastructure/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from config import get_config
+from .config import get_config
 
 
 conf = get_config("database")
--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from infastructure.database import engine, Base
 
 app = FastAPI()
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def root():
     return {"message": "Welcome!!"}
+
+@app.get("/check-db")
+async def startup_event():
+    try:
+        engine.connect()
+        print("Database connection established successfully")
+    except Exception as e:
+        print(f"Database connection error: {e}")
