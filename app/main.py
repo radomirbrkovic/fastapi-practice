2e97035d47a451eec0e2eb479559f641e82fe04e
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-09-10 15:02:06 +0200

    Create user model

--- app/main.py
+++ app/main.py
@@ -7,11 +7,3 @@ Base.metadata.create_all(bind=engine)
 @app.get("/")
 async def root():
     return {"message": "Welcome!!"}
-
-@app.get("/check-db")
-async def startup_event():
-    try:
-        engine.connect()
-        print("Database connection established successfully")
-    except Exception as e:
-        print(f"Database connection error: {e}")
