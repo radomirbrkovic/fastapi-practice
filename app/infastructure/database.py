8d6edb399b9a359c910a28cfe6f4679d9da5ac57
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2024-09-14 19:10:01 +0200

    Create user

--- app/infastructure/database.py
+++ app/infastructure/database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import get_config
 
-
-conf = get_config("database")
+conf = get_config('database')
 SQLALCHEMY_DATABASE_URL = f"postgresql://{conf['username']}:{conf['password']}@{conf['host']}/{conf['name']}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
